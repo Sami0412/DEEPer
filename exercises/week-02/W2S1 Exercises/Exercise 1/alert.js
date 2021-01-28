6569f3defe3890da946dbe333f6f883631f31721
Sami Williams <samiw0412@gmail.com>
2021-01-28 09:57:54 +0000

    W2S2 pre-lecture commit

---
+++ exercises/week-02/W2S1 Exercises/Exercise 1/alert.js
@@ -0,0 +1,16 @@
+//alert("I'm Javascript!")      Task 2
+
+//Task: "Working with Variables"
+let admin;
+let name;
+name="John";
+admin=name;
+alert(admin);
+
+//Task: "Giving the right name"
+const ourPlanet;
+let currentUser;
+
+//Task: Uppercase Const?
+const BIRTHDAY = '18.04.1982'; //value known prior to execution
+const age = someCode(birthday);
