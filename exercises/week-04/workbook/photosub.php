3fdab7d39d04dda2ccb3360064295c9b55b071e6
Sami Williams <samiw0412@gmail.com>
2021-02-23 18:42:47 +0000

    Suggested changes made & carousel CSS improved

--- exercises/week-04/workbook/photosub.php
+++ exercises/week-04/workbook/photosub.php
@@ -13,52 +13,49 @@ class Photo
     public $submitted;
 }
 
+//Only runs on form submission
 if (!empty($_POST)) {
-
+    //Hydrate Photo class with form values
     $submission = new Photo();
-    $submission->name = $_POST['name'];
+    $submission->name = $_POST['name'];     //$_POST property names come from name values on html element
     $submission->dob = $_POST['DoB'];
     $submission->email = $_POST['email'];
     $submission->submitted = Carbon::now()->timestamp;
-    //var_dump($submission);
-    };
+    }
 
+//Runs if file submitted
 if (!empty($_FILES)) {
-    //var_dump($_FILES);
-    if (!empty($_FILES['photo'])) {
+
+    if (!empty($_FILES['photo'])) { //$_FILES is an array containing name, type, tmp_name, error and size
+        //Save array to variable
         $file = $_FILES['photo'];
 
         //Error handling
         if ($file['error'] !== UPLOAD_ERR_OK) {
-           //display error msg to user e.g please choose valid file
+           //display error bootstrap alert
             $responseMsg = '<div class="alert alert-warning" role="alert">An error has occurred, please try again</div>';
         } else {
-            //validate mime type & file ext - if $file['type'] !== png/jpg etc
-            //is file unique
-
+            //Create path name e.g. 'uploads/cat.jpg
             $targetPath = 'uploads/' . $file['name'];
+            //Replace temporary file path with newly created path
             if (!move_uploaded_file(
                 $file['tmp_name'],
                 $targetPath
             )) {
                 throw new RuntimeException('Failed to move file');
             }
-
+            //Add new file path into photo class instance
             $submission->picture = $targetPath;
-            //var_dump($submission);
-
+            //Serialise & store the hydrated class instance using timestamp as name
             $serialisedSubmission = serialize($submission);
             file_put_contents("$submission->submitted" . ".txt", $serialisedSubmission);
 
-            //add to carousel
-
+            //Show bootstrap success alert on submission
             $responseMsg = '<div class="alert alert-success" role="alert">Photo uploaded successfully!</div>';
         }
     }
 }
-
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -70,40 +67,43 @@ if (!empty($_FILES)) {
     <link rel="stylesheet" href="photosub.css">
     <title>Photo Submission</title>
 </head>
-<body>
+<body class="container">
 <h1 class="text-center">Photo Submission</h1>
 <p class="text-center">Please complete all fields before submitting your photo</p>
-<div class="container m-3">
-    <form action="" method="post" enctype="multipart/form-data">
-        <div class="row centre-div">
-            <div class="form-group col-12">
-                <label for="name">Full Name:</label>
-                <input type="text" name="name" id="name" class="form-control">
+<div class="row">
+    <div class="col-12">
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="row centre-div">
+                <div class="form-group col-12">
+                    <label for="name">Full Name:</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group col-12">
+                    <label for="DoB">Date of Birth:</label>
+                    <input type="date" name="DoB" id="DoB" class="form-control">
+                </div>
+                <div class="form-group col-12">
+                    <label for="email">Email Address:</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group col-12">
+                    <label for="photo">Select photo to upload:</label>
+                    <input type="file" name="photo" id="photo" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-info btn-lg ml-3">Submit</button>
+                <?php if (isset($responseMsg)){
+                    echo $responseMsg;
+                }?>
             </div>
-            <div class="form-group col-12">
-                <label for="DoB">Date of Birth:</label>
-                <input type="date" name="DoB" id="DoB" class="form-control">
-            </div>
-            <div class="form-group col-12">
-                <label for="email">Email Address:</label>
-                <input type="email" name="email" id="email" class="form-control">
-            </div>
-            <div class="form-group col-12">
-                <label for="photo">Select photo to upload:</label>
-                <input type="file" name="photo" id="photo" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-info btn-lg ml-3">Submit</button>
-            <?php if (isset($responseMsg))
-                echo $responseMsg; ?>
-        </div>
-    </form>
-</div>
-<div class="container m-6">
-    <div class="row justify-content-center">
-        <div id="carouselExampleSlidesOnly" class="carousel slide col-12" data-ride="carousel" data-interval="3000">
-            <div class="carousel-inner col-12 mb-5">
+        </form>
+    </div>
+
+    <div class="col-12 mt-5 mb-5">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="3000">
+            <div class="carousel-inner">
                 <!--Remove hidden dot files from directory before looping-->
-                <?php $images = array_diff(scandir('uploads/', SCANDIR_SORT_NONE), array('..', '.'));
+                <?php
+                $images = array_diff(scandir('uploads/', SCANDIR_SORT_NONE), array('..', '.'));
                 //assign first element (first file in uploads) to variable as one needs to be 'active' in carousel
                 $head = $images[0];
                 echo "<div class='carousel-item active'>";
@@ -119,18 +119,14 @@ if (!empty($_FILES)) {
                     echo "<div class='carousel-item'>";
                     echo "<img src='uploads/$image' class='d-block w-100'>";
                     echo "</div>";
-                }; ?>
+                } ?>
             </div>
         </div>
     </div>
 </div>
 
-
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-<script>
-   // $('.carousel').carousel();
-</script>
 </body>
 </html>
